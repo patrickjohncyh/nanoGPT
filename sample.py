5a7f258d68deec75f14a980a72caa5ce929d1ac9
Patrick John Chia <patrickjohncyh@icloud.com>
2024-10-02 21:19:31 +0800

    only take steps after some generation

--- sample.py
+++ sample.py
@@ -116,6 +116,11 @@ x = torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...]
 with torch.no_grad():
     with ctx:
         for k in range(num_samples):
+            if is_noether:
+                if k < 10:
+                    model.inner_steps = 0
+                else:
+                    model.inner_steps = 10
             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
             print(decode(y[0].tolist()))
             print("---------------")
