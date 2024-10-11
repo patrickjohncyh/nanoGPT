d563e265a1d8b1697cbcc5770433fde972e78a53
Patrick John Chia <patrickjohncyh@icloud.com>
2024-10-11 17:57:41 +0800

    argmax sampling

--- noether.py
+++ noether.py
@@ -306,7 +306,8 @@ class Noether(nn.Module):
             # apply softmax to convert logits to (normalized) probabilities
             probs = F.softmax(logits, dim=-1)
             # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1)
+            # idx_next = torch.multinomial(probs, num_samples=1)
+            idx_next = torch.argmax(probs)[None, None]
             # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
 
